099585e4f80db7959bce53ecbb0272b0ade698db
Joe Groocock <frebib@gmail.com>
2017-11-06 22:56:56 +0000

    Add permission checks and gen-config to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
 set -e
 
+export CFG_DIR="${CFG_DIG:-/config}"
+
+if ! su-exec "$UID:$GID" touch "$CFG_DIR/.write-test"; then
+    2>&1 echo "Warning: No permission to write in '$CFG_DIR' directory."
+    2>&1 echo "         It is likely that Sonarr will have permission issues"
+    2>&1 echo
+    chown $UID:$GID "$CFG_DIR"
+    chmod o+rw "$CFG_DIR"
+fi
+# Remove temporary file
+rm -f touch "$CFG_DIR/.write-test"
+
+su-exec "$UID:$GID" gen-config.sh
+
 exec su-exec "$UID:$GID" "$@"
