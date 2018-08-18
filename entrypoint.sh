9bfd4aeacce656e6b8bbbaeb33ca4d0c59902f8f
Joe Groocock <me@frebib.net>
2018-08-18 21:58:25 +0100

    Be informative about permission changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,13 +5,16 @@ export CFG_DIR="${CFG_DIG:-/config}"
 
 if ! su-exec -e touch "$CFG_DIR/.write-test"; then
     2>&1 echo "Warning: No permission to write in '$CFG_DIR' directory."
-    2>&1 echo "         It is likely that Sonarr will have permission issues"
+    2>&1 echo "         Correcting permissions to prevent a crash"
     2>&1 echo
+    (
+    set -x
     chown $SUID:$SGID "$CFG_DIR"
     chmod o+rw "$CFG_DIR"
+    )
 fi
 # Remove temporary file
-rm -f touch "$CFG_DIR/.write-test"
+rm -f "$CFG_DIR/.write-test"
 
 su-exec -e gen-config.sh
 
